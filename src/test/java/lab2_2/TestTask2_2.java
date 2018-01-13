475d468457b17036e13093e1294bebe4d266d1ff
Strek.Andrzej <198173@edu.p.lodz.pl>
2018-01-13 17:20:17 +0100

    dodanie testow dodawania klasy Money+poprawa poprzendiego commita

--- src/test/java/lab2_2/TestTask2_2.java
+++ src/test/java/lab2_2/TestTask2_2.java
@@ -15,14 +15,29 @@ public class TestTask2_2 {
     }
 
     @org.junit.Test
-    public void monyeMultiplyingByTwo() {
+    public void moneyMultiplyingByTwo() {
         Money money = new Money(120, "PLN");
 
         assertThat(money.multiplyBy(2), Matchers.is(new Money(240, "PLN")));
     }
 
-    public void monyeDivideByTwoSecondTwoParametersConstructor() {
+    @org.junit.Test
+    public void moneyDivideByTwoSecondTwoParametersConstructor() {
         Money money = new Money(120);
-        assertThat(money.multiplyBy(1 / 2), Matchers.is(new Money(60, "PLN")));
+        assertThat(money.multiplyBy(0.5), Matchers.is(new Money(60)));
+    }
+
+    @org.junit.Test
+    public void moneyAddingTwoArguments() {
+        Money money1arg = new Money(120, "PLN");
+        Money money2arg = new Money(240, "PLN");
+        assertThat(money1arg.add(money2arg), Matchers.is(new Money(360, "PLN")));
+    }
+
+    @org.junit.Test
+    public void moneyAddingWithDefaultCurrency() {
+        Money money1arg = new Money(60);
+        Money money2arg = new Money(20);
+        assertThat(money1arg.add(money2arg), Matchers.is(new Money(80, "EUR")));
     }
 }
