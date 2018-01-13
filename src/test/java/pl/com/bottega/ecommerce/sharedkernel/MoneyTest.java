19deb30f3a7412cae563b448ba30b8d6d9320baa
Chojnacki.Piotr <198144@edu.p.lodz.pl>
2018-01-13 17:17:50 +0100

    add test for subtract method from Money class

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -11,16 +11,24 @@ import static org.junit.Assert.assertThat;
 public class MoneyTest {
 
     @Test
-    public void MultiplyByFiveIsCorrect() {
+    public void multiplyByFiveIsCorrect() {
         Money money = new Money(new BigDecimal(2), Currency.getInstance("PLN"));
         assertThat(money.multiplyBy(5), Matchers.is(new Money(new BigDecimal(10), Currency.getInstance("PLN"))));
     }
 
     @Test
-    public void AddSixEuroToFiveEuro() {
+    public void addSixEuroToFiveEuro() {
         Money money = new Money(new BigDecimal(5), Currency.getInstance("EUR"));
         Money addMoney = new Money(new BigDecimal(6), Currency.getInstance("EUR"));
         Money expectedMoney = new Money(new BigDecimal(11), Currency.getInstance("EUR"));
         assertThat(money.add(addMoney), Matchers.is(expectedMoney));
+    }
+
+    @Test
+    public void subtractSevenEuroFromTenEuro(){
+        Money money = new Money(new BigDecimal(10), Currency.getInstance("EUR"));
+        Money subtractMoney = new Money(new BigDecimal(7), Currency.getInstance("EUR"));
+        Money expectedMoney = new Money(new BigDecimal(3), Currency.getInstance("EUR"));
+        assertThat(money.subtract(subtractMoney), Matchers.is(expectedMoney));
     }
 }
