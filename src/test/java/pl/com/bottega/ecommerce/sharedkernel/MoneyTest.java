ef7ee254a5012eeec8aae17fe11ec89a11f160e1
Kaminski.Lukasz <198151@edu.p.lodz.pl>
2018-01-13 16:52:58 +0100

    [lab_2_2] addTestPositive for Money class

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 public class MoneyTest {
 
     @Test
-    public void multiplyTest() {
+    public void multiplyTestPositive() {
         BigDecimal actualValue = new BigDecimal(100);
         BigDecimal expectedValue = new BigDecimal(500);
         BigDecimal multiplyier = new BigDecimal(5);
@@ -20,6 +20,18 @@ public class MoneyTest {
         Money expectedMoney = new Money(expectedValue, currency);
 
         assertThat(actualMoney.multiplyBy(multiplyier), Matchers.is(expectedMoney));
+    }
+
+    @Test
+    public void addTestPositive() {
+        BigDecimal actualValue = new BigDecimal(100);
+        BigDecimal expectedValue = new BigDecimal(200);
+        Currency currency = Currency.getInstance("USD");
+        Money actualMoneyFirst = new Money(actualValue, currency);
+        Money actualMoneySecond = new Money(actualValue, currency);
+        Money expectedMoney = new Money(expectedValue, currency);
+
+        assertThat(actualMoneyFirst.add(actualMoneySecond), Matchers.is(expectedMoney));
     }
 
 }
