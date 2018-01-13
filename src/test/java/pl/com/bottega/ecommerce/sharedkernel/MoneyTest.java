87fb5c0a3c172b340017e43eebe446c1abc12f0e
Chojnacki.Piotr <198144@edu.p.lodz.pl>
2018-01-13 17:11:00 +0100

    add test for add method from Money class

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -5,13 +5,22 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+
 import static org.junit.Assert.assertThat;
 
 public class MoneyTest {
 
     @Test
-    public void MultiplyByFiveIsCorrect(){
+    public void MultiplyByFiveIsCorrect() {
         Money money = new Money(new BigDecimal(2), Currency.getInstance("PLN"));
         assertThat(money.multiplyBy(5), Matchers.is(new Money(new BigDecimal(10), Currency.getInstance("PLN"))));
+    }
+
+    @Test
+    public void AddSixEuroToFiveEuro() {
+        Money money = new Money(new BigDecimal(5), Currency.getInstance("EUR"));
+        Money addMoney = new Money(new BigDecimal(6), Currency.getInstance("EUR"));
+        Money expectedMoney = new Money(new BigDecimal(11), Currency.getInstance("EUR"));
+        assertThat(money.add(addMoney), Matchers.is(expectedMoney));
     }
 }
