b2e1370da42467a6565ea81ef63096237e98b092
jankowski.lukasz <191867@edu.p.lodz.pl>
2018-01-14 20:59:17 +0100

    add addMoneyWithDenominationNotSameCurrencyCode test

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -3,6 +3,7 @@ package pl.com.bottega.ecommerce.sharedkernel;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import java.lang.reflect.Method;
 import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
@@ -18,6 +19,12 @@ public class MoneyTest {
         assertThat(money.multiplyBy(10), Matchers.is(new Money(1000, "GBH")));
     }
 
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addMoneyWithDenominationNotSameCurrencyCode(){
+        Money money = new Money(100, "PLN");
+        Money result = money.add(new Money(100,"GBH"));
+    }
 
 
 }
