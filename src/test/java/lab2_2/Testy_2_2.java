c3801d98537c107257c7b0a28d545c8ceb9e17bc
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-01-16 18:08:20 +0100

    Dodanie testu dzielenia

--- src/test/java/lab2_2/Testy_2_2.java
+++ src/test/java/lab2_2/Testy_2_2.java
@@ -1,16 +1,22 @@
 package lab2_2;
 
-import static org.junit.Assert.assertThat;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import static org.junit.Assert.assertThat;
+
 public class Testy_2_2 {
 
-        @Test
-        public void testShouldGiveAnswerMultiplyByTwo() {
-            Money money = new Money(100, "PLN");
-            assertThat(money.multiplyBy(2), Matchers.is(new Money(200, "PLN")));
-        }
+    @Test public void testShouldGiveAnswerMultiplyByTwo() {
+        Money money = new Money(100, "PLN");
+        assertThat(money.multiplyBy(2), Matchers.is(new Money(200, "PLN")));
+    }
+
+    @Test public void testShouldGiveMoneyDivideByTwoSecondTwoParametersConstructor() {
+        Money money = new Money(120);
+        assertThat(money.multiplyBy(0.5), Matchers.is(new Money(60)));
+    }
+
 
 }
