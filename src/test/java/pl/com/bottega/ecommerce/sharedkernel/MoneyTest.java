fb4c72d251f1ef86b2145b2438acf836d7d133bd
oktawianmankowski <198162@edu.p.lodz.pl>
2018-01-29 02:03:56 +0100

    test of multiply method from Money class

---
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,18 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.Assert.assertThat;
+
+public class MoneyTest {
+
+    @Test
+    public void multiplyByFiveIsCorrect() {
+        Money money = new Money(new BigDecimal(2), Currency.getInstance("PLN"));
+        assertThat(money.multiplyBy(5), Matchers.is(new Money(new BigDecimal(10), Currency.getInstance("PLN"))));
+    }
+}
